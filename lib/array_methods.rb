94d6ed3a46886b71d1adf9a59d72e706b01ad61d
Arina Victor <arinavictor@yahoo.com>
2019-08-12 19:49:33 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,18 @@
+#use loop to find the position(index) of a value(element)
+#find the maximumm value 
+#find min value 
+#all arrays are positive integers 
+
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+  # Add your solution here	 
+  counter = 0
+  while array[counter] != array[value_to_find]
+  counter ++
+  end 
+
+  return array[value_to_find]
+end	
 
 def find_max_value(array)
   # Add your solution here
